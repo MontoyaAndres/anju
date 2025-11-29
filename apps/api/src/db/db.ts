368bddfc5abcf70ab4d0703674ced69e4fbae21b
MontoyaAndres <andresmontoyafcb@gmail.com>
2025-11-29 07:31:33 -0500

    Feat: psql connector

--- apps/api/src/db/db.ts
+++ apps/api/src/db/db.ts
@@ -1,14 +1,16 @@
 import { Context } from 'hono';
-import { Pool } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
+import postgres from 'postgres';
+import { drizzle } from 'drizzle-orm/postgres-js';
 
 import * as schema from './schema';
 import { getDBConnectionString } from '../utils';
 
 export const createDb = (c: Context) => {
   const connectionString = getDBConnectionString(c.env);
-  const pool = new Pool({ connectionString });
-  return drizzle(pool, { schema });
+  const client = postgres(connectionString, {
+    prepare: true,
+  });
+  return drizzle(client, { schema });
 };
 
 export type Database = ReturnType<typeof createDb>;
